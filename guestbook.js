f5878f8a94765d1218299be39e32f1558c38d51c
Diego Abete <54179001+abetediego@users.noreply.github.com>
2022-06-14 19:47:27 -0300

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://8bf25caf.us-south.apigw.appdomain.cloud/guestbook/';
+const apiUrl = 'https://8bf25caf.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
